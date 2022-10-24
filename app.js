f4ebf363c5921fcfda826dc14a2306c63a7e2f95
Danil_Beliy <danil22344bel@gmail.com>
2022-10-24 22:38:45 +0300

    6 commit

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 $(function () {
+    let header = $("#header");
+    let headerH = header.height();
+
+    /* Scroll */
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+        let elId = $(this).data('scroll');
+        let elOffset = $(elId).offset().top;
+        headerH = header.height();
+
+        $("html, body").animate({
+            scrollTop: elOffset - headerH
+        });
+    });
+
+
     /* Blog Slider */
     let slider = $("#blogSlider");
-
     slider.slick({
         infinite: true,
         slidersToShow: 1,
@@ -11,4 +26,20 @@ $(function () {
         dots: true
     });
 
+    /* Scroll Up */
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
+
+    $('.scroll-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 });
+
